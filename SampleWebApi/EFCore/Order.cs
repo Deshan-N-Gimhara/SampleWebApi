05443838e99ac1abf4bd085c151882f78a2d3eb7
Deshan-N-Gimhara <87584743+Deshan-N-Gimhara@users.noreply.github.com>
2024-04-08 19:29:06 +0530

    Add project files.

---
+++ SampleWebApi/EFCore/Order.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleWebApi.EFCore
+{
+    [Table("order")]
+    public class Order
+    {
+        [Key, Required]
+        public int Id { get; set; }
+        public int Product_id { get; set; }
+        public virtual Product Product { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone {  get; set; }
+
+    }
+}
---
+++ SampleWebApi/EFCore/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleWebApi.EFCore
+{
+    [Table("product")]
+    public class Product
+    {
+        [Key,Required]
+        public int Id {  get; set; }
+        public required string Name {  get; set; }
+        public required string Brand {  get; set; }
+        public int Size {  get; set; }
+        public decimal Price { get; set; }
+    }
+}
